8d8418d452f18dfadc21fdfa84f47693935ed9e5
jtoallen@gmail.com <allenja2@oregonstate.edu>
2022-02-24 14:00:59 -0600

    removed while loop

--- numbers.py
+++ numbers.py
@@ -6,8 +6,11 @@ based on which lotto game the user selects. Writes numbers to a text file
 numbers.txt
 """
 
+import os
 import random
 import time
+import contextlib
+
 
 
 def random_number():
@@ -15,45 +18,59 @@ def random_number():
 
     random_nums = []
 
-    while True:
+    # while True:
     #     time.sleep(1)
+    #     random_nums = []
 
-        with open('lottoType.txt', 'r') as infile:
-            condition = infile.readline()
-            condition = condition.strip()
-            print(condition)  #testing code
+        # with contextlib.suppress(FileNotFoundError):
+        #     os.remove('numbers.txt')
+        #     print('file removed')
 
-            if condition == '1':
-                #conditions for Powerball
-                with open('numbers.txt', 'w') as outfile:
-                    for i in range(5):
-                        random_nums.append(random.randint(1, 69))
-                    random_nums.append(random.randint(1,26))
-                    # print("Powerball", random_nums)
-                    outfile.write(str(random_nums))
 
-            elif condition == '2':
-                #conditions for Mega Millions
-                with open('numbers.txt', 'w') as outfile:
-                    for i in range(5):
-                        random_nums.append(random.randint(1, 70))
-                    random_nums.append(random.randint(1,25))
-                    # print("Mega Millions", random_nums)
-                    outfile.write(str(random_nums))
+    with open('lottoType.txt', 'r') as infile:
+        condition = infile.readline()
+        condition = condition.strip()
+        print(condition)  #testing code
+        # with contextlib.suppress(FileNotFoundError):
+        #     os.remove('numbers.txt')
+        #     print('file removed')
 
-            elif condition == '3':
-                #conditions for Powerball with winning hockey score
-                with open('numbers.txt', 'w') as outfile:
-                    for i in range(5):
-                        random_nums.append(random.randint(1, 70))
-                    # random_nums.append(random.randint(1,25))
-                    with open('hockeyscore.txt', 'r') as inscore:
-                        score = inscore.readline()
-                        score = int(score.strip())
-                        # print(score)
-                        random_nums.append(score)
-                    # print("Powerball", random_nums)
-                    outfile.write(str(random_nums))
+
+        if condition == '1':
+            with open('numbers.txt', 'w') as outfile:
+                for i in range(5):
+                    random_nums.append(random.randint(1, 69))
+                random_nums.append(random.randint(1,26))
+                print("Powerball", random_nums)
+                outfile.write(str(random_nums))
+                # time.sleep(1)
+                # with contextlib.suppress(FileNotFoundError):
+                #     os.remove('lottoType.txt')
+                #     print('file removed')
+
+        elif condition == '2':
+            #conditions for Mega Millions
+            with open('numbers.txt', 'w') as outfile:
+                for i in range(5):
+                    random_nums.append(random.randint(1, 70))
+                random_nums.append(random.randint(1,25))
+                # print("Mega Millions", random_nums)
+                outfile.write(str(random_nums))
+
+        elif condition == '3':
+            #conditions for Powerball with winning hockey score
+            with open('numbers.txt', 'w') as outfile:
+                for i in range(5):
+                    random_nums.append(random.randint(1, 70))
+                # random_nums.append(random.randint(1,25))
+                with open('hockeyscore.txt', 'r') as inscore:
+                    score = inscore.readline()
+                    score = int(score.strip())
+                    # print(score)
+                    random_nums.append(score)
+                # print("Powerball", random_nums)
+                outfile.write(str(random_nums))
+
 
 
 def main():
