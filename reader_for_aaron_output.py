b55feec220fc56b970c4b9c7ecf9e2a5aefe2b3a
jtoallen@gmail.com <allenja2@oregonstate.edu>
2022-03-17 12:09:34 -0500

    removed comments and print statement

--- reader_for_aaron_output.py
+++ reader_for_aaron_output.py
@@ -1,6 +1,5 @@
 import csv
 
-
 with open('/Users/jason/cs361/Aaron_Project/output.txt') as fin, \
         open('output.csv', 'wb') as fout:
         csvin = csv.reader(fin)
@@ -8,17 +7,13 @@ with open('/Users/jason/cs361/Aaron_Project/output.txt') as fin, \
 
         for row in csvin:
             if row[1].strip(" ' ") == 'Toronto Maple Leafs':
-                print("The Toronto Maple Leafs score is", row[2])
                 score = row[2].strip("[]")
                 score = score.strip()
                 with open('hockeyscore.txt', 'w') as inscore:
                     inscore.write(score)
             elif row[3].strip(" ' ") == 'Toronto Maple Leafs':
                 score = row[4].strip("[]")
-                print("Toronto Maple Leafs score is", score)
                 score = score.strip()
                 with open('hockeyscore.txt', 'w') as inscore:
                     inscore.write(score)
-
-
 
