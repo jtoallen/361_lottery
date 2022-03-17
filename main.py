2d2a620615c79d46a8303965045d0de9163eed69
jtoallen@gmail.com <allenja2@oregonstate.edu>
2022-03-17 10:41:38 -0500

    restructured user interace to only take input and read responses. also redefined loop condition in user interface.  now it calls a function to check whether condition 4 has been entered. condition 4 exits program.  created new functions for each condition: 1,2,3, and 4.  each has unique function that gives reponse to user_interface.  adheres to encapsulation principles and remove smelly functions meaning shorter functions with clearly defined tasks.

--- main.py
+++ main.py
@@ -20,12 +20,80 @@ import subprocess
 cmd = 'python numbers.py'
 new_pipe = subprocess.Popen(cmd, shell= True)
 
+def true_loop_condition():
+    return True
+
+def false_loop_condition():
+    return False
+
+
+def generate_powerball_nums():
+    """generates powerball random numbers"""
+
+    new_pipe = subprocess.Popen(cmd, shell=True)
+    with open('lottoType.txt', 'w') as out:
+        out.write("1")
+
+    with open('numbers.txt', 'r') as infile:
+        num = infile.readline()
+        num = num.strip()
+        print("These are your lucky POWERBALL numbers", num)
+        print("\n")
+
+
+def generate_mega_millions_nums():
+    """generates set of random mega millions numbers"""
+    new_pipe = subprocess.Popen(cmd, shell=True)
+    with open('lottoType.txt', 'w') as out:
+        out.write("2")
+
+    with open('numbers.txt', 'r') as infile:
+        num = infile.readline()
+        num = num.strip()
+        print("These are your lucky MEGA MILLIONS numbers", num)
+        print("\n")
+
+
+def generate_hockey_score_powerball_nums():
+    """generates random set of powerball nums with hockey score as powerball num"""
+    subprocess.Popen(cmd, shell=True)
+    random_nums = []
+    with open('numbers.txt', 'w') as outfile:
+        for i in range(5):
+            random_nums.append(random.randint(1, 70))
+        with open('hockeyscore.txt', 'r') as inscore:
+            score = inscore.readline()
+            score = int(score.strip())
+            random_nums.append(score)
+
+        with open('numbers.txt', 'w') as outfile:
+            outfile.write(str(random_nums))
+
+        with open('numbers.txt', 'r') as infile:
+            num = infile.readline()
+            num = num.strip()
+            print("These are your lucky POWERBALL numbers with the "
+                  "Toronto Maple Leafs game score as your powerball number",
+                  num)
+            print("\n")
+
+
+def exit_program():
+    """stops main program and exits"""
+    with open('lottoType.txt', 'w') as out:
+        out.write("4")
+    print("You have chosen to EXIT the program. Goodbye.\n")
+    return false_loop_condition()
+
+
+def invalid_entry():
+    print("INVALID ENTRY.\n")
+
 
 def user_interface():
     """user interface takes input reads response and outputs lotto numbers"""
 
-
-    while True:
+    while true_loop_condition():
         response = input("Enter 1 to generate your lucky numbers for Texas Powerball\n"
                          "or\n"
         "Enter 2 to generate your lucky numbers for Texas Mega Millions\n"
@@ -36,72 +104,17 @@ def user_interface():
         "Enter 4 to exit program: \n")
 
         if response == '1':
-            """
-            1-write '1' in  lottoType.txt "
-            2-sleep for 5 seconds
-            3-read and output to lottoType.txt file
-            """
-            new_pipe = subprocess.Popen(cmd, shell=True)
-
-            with open('lottoType.txt', 'w') as out:
-                out.write("1")
-
-            with open('numbers.txt', 'r') as infile:
-                num = infile.readline()
-                num = num.strip()
-                print("These are your lucky POWERBALL numbers", num)
-                print("\n")
-
+            generate_powerball_nums()
         elif response == '2':
-            new_pipe = subprocess.Popen(cmd, shell=True)
-            with open('lottoType.txt', 'w') as out:
-                out.write("2")
-
-            with open('numbers.txt', 'r') as infile:
-                num = infile.readline()
-                num = num.strip()
-                print("These are your lucky MEGA MILLIONS numbers", num)
-                print("\n")
-
+            generate_mega_millions_nums()
         elif response == '3':
-            subprocess.Popen(cmd, shell=True)
-            random_nums = []
-            with open('numbers.txt', 'w') as outfile:
-                for i in range(5):
-                    random_nums.append(random.randint(1, 70))
-                with open('hockeyscore.txt', 'r') as inscore:
-                    score = inscore.readline()
-                    score = int(score.strip())
-                    random_nums.append(score)
-
-                with open('numbers.txt', 'w') as outfile:
-                    outfile.write(str(random_nums))
-
-                with open('numbers.txt', 'r') as infile:
-                    num = infile.readline()
-                    num = num.strip()
-                    print("These are your lucky POWERBALL numbers with the "
-                    "Toronto Maple Leafs game score as your powerball number", num)
-                    print("\n")
-
+            generate_hockey_score_powerball_nums()
         elif response == '4':
-            with open('lottoType.txt', 'w') as out:
-                out.write("4")
-            print("You have chosen to exit the program. Goodbye.")
-            return
-
+            exit_program()
+            return false_loop_condition()
         else:
-            print("Invalid entry.  Please enter 1, 2, or 3 to generate "
-                  "lottery numbers.  To Exit, enter 4 \n")
-            response = input(
-                "Enter 1 to generate your lucky numbers for Texas Powerball\n"
-                "or\n"
-                "Enter 2 to generate your lucky numbers for Texas Mega Millions\n"
-                "or\n"
-                "Enter 3 to use the Toronto Maple Leafs game score as your "
-                "powerball number for Powerball\n"
-                "or\n"
-                "Enter 4 to exit program: \n")
+            invalid_entry()
+            user_interface()
 
 
 def main():
